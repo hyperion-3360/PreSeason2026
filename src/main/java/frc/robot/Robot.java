83c58400d45e1cfcf2a078242e54bc808bd34761
Tonytheclutcher <35316132+AntoineCrete@users.noreply.github.com>
2025-10-22 22:08:07 -0400

    add exponential scale

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,6 +20,7 @@ public class Robot extends TimedRobot {
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
+        m_robotContainer.periodic(); // Update battery voltage monitoring
     }
 
     @Override
