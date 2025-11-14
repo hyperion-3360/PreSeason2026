69de89870b9c3ffea80ae64fd31e67946a1b2769
Étienne <cote04a1@eleve.csrs.qc.ca>
2025-11-13 19:58:38 -0500

    Scorekeeper
    Tool to calculate optimal scoring

---
+++ src/main/java/frc/robot/subsystems/util/Scorekeeper/Scorekeeper.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.util.Scorekeeper;
+
+/** Scorekeeper is a tool to calculate the most efficient next step for optimal scoring.
+ * <p> Currently "built" for 2025*/
+public class Scorekeeper {
+    
+}
