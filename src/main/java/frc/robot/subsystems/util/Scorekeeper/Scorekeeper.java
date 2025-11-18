65696103e21608a49e541cb0d7395fd2eaadaf36
Étienne <cote04a1@eleve.csrs.qc.ca>
2025-11-17 19:04:00 -0500

    Added coral placement tracking

--- src/main/java/frc/robot/subsystems/util/Scorekeeper/Scorekeeper.java
+++ src/main/java/frc/robot/subsystems/util/Scorekeeper/Scorekeeper.java
@@ -3,5 +3,10 @@ package frc.robot.subsystems.util.Scorekeeper;
 /** Scorekeeper is a tool to calculate the most efficient next step for optimal scoring.
  * <p> Currently "built" for 2025*/
 public class Scorekeeper {
-    
+    /** Reef (except for L1)
+     * @param first index: level (0 = L4, 1 = L3, 2 = L2)
+     * @param second index: peg number (0-7 clockwise from left one on the barge side of the reef) */
+    boolean[][] reef = new boolean[3][8];    
+    /** Counts the coral on each  side, starting from the closest to the barge */
+    public int[] reefL1 = new int[6]; // Counts number per side
 }
---
+++ src/main/java/frc/robot/subsystems/util/Scorekeeper/SkTodo.md
@@ -0,0 +1,10 @@
+Create the constants (DONE)
+Add coral placcement tracker (DONE)
+Add processor and net algae counter
+Add simple positions (preset points : less precise, easier to code)
+Add basic "efficient step finder" 
+Find a better name for the "efficient step finder"
+Make the step finder more precise (kinda like a chess engine)
+
+Maybe :
+Add actual position (coordinates) and paths for said positions
